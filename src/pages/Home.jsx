b7cfefddb590c6a62dee5ef18617cf304a11c66e
ranjit <ranjitsantoshrai47@gmail.com>
2025-10-17 10:44:20 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
-import BlogList from '../components/Bloglist'
+import BlogList from '../components/BlogList'
 import Newslatter from '../components/Newslatter'
 import Footer from '../components/Footer'
 
